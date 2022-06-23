f99f9842852dbf2ff5d3ab84094a60acbf9c8747
MohanRaj <mraj86698@gmail.com>
2022-06-23 17:42:07 +0530

    Added Employee Attendance Check

--- src/empwage/EmployeeWageComputation.java
+++ src/empwage/EmployeeWageComputation.java
@@ -3,9 +3,9 @@ package empwage;
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int employeePresent = 1;
-		int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
-		if (isPresent == employeePresent) {
+		int full_Time = 1;
+		int empType = (int) (Math.floor(Math.random() * 10) % 2);
+		if (empType == full_Time) {
 			System.out.println("Employee is Present");
 		} else {
 			System.out.println("Employee is Absent");
