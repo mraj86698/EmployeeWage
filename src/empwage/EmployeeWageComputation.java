7ca4f78a6d3277c96af0d38c7c4b34ed72fa1a15
MohanRaj <mraj86698@gmail.com>
2022-06-23 17:14:48 +0530

    Added Wage Calculation till max hours or max days required

--- src/empwage/EmployeeWageComputation.java
+++ src/empwage/EmployeeWageComputation.java
@@ -2,34 +2,37 @@ package empwage;
 
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		System.out.println("Welcome to Employee Wage Computation Program\n");
 		final int part_Time = 1;
 		final int full_Time = 2;
 		final int wage_per_Hour = 20;
-		final int working_Days = 20;
+		final int max_working_Days = 20;
+		final int max_working_Hours = 100;
 		
 		int total_Wage = 0;
-		for(int day=0;day <= working_Days;day++) {
+		int working_Hours = 0;
+		System.out.printf("%5s		%5s		%5s		%5s\n","Day","Working_Hours","Wage","Total_Working_Hours\n\n");
+		for(int day=1,total_working_Hours = 0;day <= max_working_Days && total_working_Hours < max_working_Hours; day++,total_working_Hours +=working_Hours) {
 		
 		int empType = (int) (Math.random() * 100) % 3;
-		int working_Hours = 0;
+		
 		switch (empType) {
 		case full_Time:
-			System.out.println("Employee is Present FullTime");
+			System.out.println("Employee is Present FullTime\n");
 			working_Hours = 8;
 			break;
 		case part_Time:
-			System.out.println("Employee Present PartTime");
+			System.out.println("Employee Present PartTime\n");
 			working_Hours = 4;
 			break;
 		default:
-			System.out.println("Employee is Absent");
+			System.out.println("Employee is Absent\n");
 
 		}
 		int wage = working_Hours * wage_per_Hour;
-		System.out.println("Day "+day+" Wage is "+ wage);
-		total_Wage += wage;
 		
+		total_Wage += wage;
+		System.out.printf("%5d		%5d		%5d		%5d\n",day,working_Hours,wage,total_working_Hours + working_Hours);
 		}
 		System.out.println("Total wage for a month is "+ total_Wage);
 
