af5efc61e2a8c5ba58e808ec96af8dcbeeb19de9
MohanRaj <mraj86698@gmail.com>
2022-06-23 17:58:03 +0530

    Added Daily Wage Calculation

--- src/empwage/EmployeeWageComputation.java
+++ src/empwage/EmployeeWageComputation.java
@@ -3,11 +3,11 @@ package empwage;
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		final int employeePresent = 1;
+		final int full_Time = 1;
 		final int wage_per_Hour = 20;
 		int working_Hours = 0;
-		int isPresent = (int) (Math.random() * 100) % 2;
-		if (isPresent == employeePresent) {
+		int empType = (int) (Math.random() * 100) % 2;
+		if (empType == full_Time) {
 			System.out.println("Employee is Present");
 			working_Hours=8;
 		} else {
