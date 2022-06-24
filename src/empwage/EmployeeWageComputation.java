5ffbe7fdccd2d9e29e664a9d6b79d739d232dd24
mraj86698 <81469369+mraj86698@users.noreply.github.com>
2022-06-24 12:42:27 +0530

    Update EmployeeWageComputation.java
    updated

--- src/empwage/EmployeeWageComputation.java
+++ src/empwage/EmployeeWageComputation.java
@@ -2,14 +2,14 @@ package empwage;
 
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
-// 		System.out.println("Welcome to Employee Wage Computation Program");
-// // 		int full_Time = 1;
-// // 		int empType = (int) (Math.floor(Math.random() * 10) % 2);
-// 		if (empType == full_Time) {
-// 			System.out.println("Employee is Present");
-// 		} else {
-// 			System.out.println("Employee is Absent");
-// 		}
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int full_Time = 1;
+		int empType = (int) (Math.floor(Math.random() * 10) % 2);
+		if (empType == full_Time) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 
 	}
 }
