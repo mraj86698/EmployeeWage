f8477f67f015da5f5586c3532d4d52c12eda96b0
MohanRaj <mraj86698@gmail.com>
2022-06-25 20:06:50 +0530

    added daily wage calculation

--- src/empwage/EmployeeWageComputation.java
+++ src/empwage/EmployeeWageComputation.java
@@ -1,40 +1,22 @@
 package empwage;
 
 public class EmployeeWageComputation {
-	public static void main(String args[]) {
-		System.out.println("Welcome to Employee Wage Computation Program\n");
-		final int part_Time = 1;
-		final int full_Time = 2;
-		final int wage_per_Hour = 20;
-		final int max_working_Days = 20;
-		final int max_working_Hours = 100;
+	public static void main(String args[])
+    {
+        final int FULL_TIME = 1;
+        final int WAGE_PER_HR = 20;
+        int empType = (int) (Math.random() * 100) % 2;
+        int workingHours = 0;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+            workingHours = 8;
+        } else
+        {
+            System.out.println("Employee is Absent");
+        }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
+    }
 		
-		int total_Wage = 0;
-		int working_Hours = 0;
-		System.out.printf("%5s		%5s		%5s		%5s\n","Day","Working_Hours","Wage","Total_Working_Hours\n\n");
-		for(int day=1,total_working_Hours = 0;day <= max_working_Days && total_working_Hours < max_working_Hours; day++,total_working_Hours +=working_Hours) {
-		
-		int empType = (int) (Math.random() * 100) % 3;
-		
-		switch (empType) {
-		case full_Time:
-			System.out.println("Employee is Present FullTime\n");
-			working_Hours = 8;
-			break;
-		case part_Time:
-			System.out.println("Employee Present PartTime\n");
-			working_Hours = 4;
-			break;
-		default:
-			System.out.println("Employee is Absent\n");
-
-		}
-		int wage = working_Hours * wage_per_Hour;
-		
-		total_Wage += wage;
-		System.out.printf("%5d		%5d		%5d		%5d\n",day,working_Hours,wage,total_working_Hours + working_Hours);
-		}
-		System.out.println("Total wage for a month is "+ total_Wage);
-
-	}
 }
